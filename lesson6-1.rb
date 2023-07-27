fd7c7dee41000120f6a003db7b86e496ef4369d1
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-07-27 12:16:17 +0900

    complete_chapter2-6

---
+++ lesson6-1.rb
@@ -0,0 +1,7 @@
+puts "計算を始めます"
+puts "２つの値を入力して下さい"
+input1 = gets.to_i
+input2 = gets.to_i
+puts "計算結果を出力します"
+puts "#{input1} * #{input2} = #{input1 * input2}"
+puts "計算を終了します"
---
+++ lesson6-2.rb
@@ -0,0 +1,19 @@
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+repeat = gets.to_i
+i = 1
+for i in 1..repeat
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します"
+  puts "#{a} + #{b} = #{a + b}"
+  puts "#{a} - #{b} = #{a - b}"
+  puts "#{a} * #{b} = #{a * b}"
+  puts "#{a} / #{b} = #{a / b}"
+  i += 1
+end
+puts "計算を終了します"
